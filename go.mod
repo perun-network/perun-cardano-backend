f700db0cabac02c0d413c03b6a425640ee717ac1
Jan Bormet <jan.bormet@bormet.info>
2022-12-12 17:10:19 +0100

    address: Add Getters for PubKeyHash, testnet address string and mainnet address string

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module perun.network/perun-cardano-backend
 go 1.17
 
 require (
+	github.com/btcsuite/btcutil v1.0.2
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
 	perun.network/go-perun v0.10.6
+	polycry.pt/poly-go v0.0.0-20220222131629-aa4bdbaab60b
 )
 
 require (
@@ -15,5 +17,4 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
-	polycry.pt/poly-go v0.0.0-20220222131629-aa4bdbaab60b // indirect
 )
--- wallet/address/address.go
+++ wallet/address/address.go
@@ -3,11 +3,14 @@ package address
 import (
 	"encoding/hex"
 	"fmt"
+	"github.com/btcsuite/btcutil/bech32"
+	"golang.org/x/crypto/blake2b"
 	"perun.network/go-perun/wallet"
 )
 
 // PubKeyLength is the length of the public verification key part of a Cardano `ed25519` keypair in bytes.
 const PubKeyLength = 32
+const PubKeyHashLength = 28
 
 // Address carries a public key that represents the public verification key part of a Cardano `ed25519` keypair
 type Address struct {
@@ -59,6 +62,69 @@ func (a *Address) UnmarshalBinary(data []byte) error {
 // String returns the public key as hex string.
 func (a Address) String() string {
 	return hex.EncodeToString(a.pubKey[:])
+}
+
+// GetTestnetAddress returns the testnet address string representation of this Address (i.e. `addr_test1...`).
+func (a Address) GetTestnetAddress() (string, error) {
+	const testnetIdentifierByte byte = 0x60
+	const testnetIdentifierString = "addr_test"
+	return a.convertToAddress(testnetIdentifierByte, testnetIdentifierString)
+}
+
+// GetMainnetAddress returns the mainnet address string representation of this Address (i.e. `addr1...`).
+func (a Address) GetMainnetAddress() (string, error) {
+	const mainnetIdentifierByte byte = 0x61
+	const mainnetIdentifierString = "addr"
+	return a.convertToAddress(mainnetIdentifierByte, mainnetIdentifierString)
+}
+
+// convertToAddress returns the address string for given network parameters.
+func (a Address) convertToAddress(networkIdentifierByte byte, networkIdentifierString string) (string, error) {
+	pubKeyHash, err := a.GetPubKeyHash()
+	if err != nil {
+		return "", fmt.Errorf("unable to compute PubKeyHash: %w", err)
+	}
+
+	// Bech32-encode the PubKeyHash.
+	conv, err := bech32.ConvertBits(append([]byte{networkIdentifierByte}, pubKeyHash[:]...), 8, 5, true)
+	if err != nil {
+		return "", fmt.Errorf("unable to convert bits for bech32 encoding: %w", err)
+	}
+	encodedHash, err := bech32.Encode(networkIdentifierString, conv)
+	if err != nil {
+		return "", fmt.Errorf("unable bech32-encode: %w", err)
+	}
+	return encodedHash, nil
+}
+
+// GetPubKeyHash returns the blake2b224-hash of the public key associated with this address.
+func (a Address) GetPubKeyHash() ([PubKeyHashLength]byte, error) {
+	// Get a blake2b-224 hash function instance.
+	blake2b224, err := blake2b.New(28, nil)
+	if err != nil {
+		return [PubKeyHashLength]byte{}, fmt.Errorf("unable to create blake2b-224 instance: %w", err)
+	}
+	// Compute the hash of the public key
+	n, err := blake2b224.Write(a.GetPubKeySlice())
+	if err != nil {
+		return [PubKeyHashLength]byte{}, fmt.Errorf("unable to compute blake2b224 hash of public key: %w", err)
+	}
+	if n != PubKeyLength {
+		return [PubKeyHashLength]byte{}, fmt.Errorf("did not write enough bytes. expected: %d, wrote: %d", PubKeyLength, n)
+	}
+	res := blake2b224.Sum(nil)
+	if len(res) != PubKeyHashLength {
+		return [PubKeyHashLength]byte{}, fmt.Errorf(
+			"hash does not have expected length. expected: %d, actual: %d",
+			PubKeyHashLength,
+			len(res),
+		)
+	}
+	ret := [PubKeyHashLength]byte{}
+
+	copy(ret[:], res)
+	// Read and return the hash.
+	return ret, nil
 }
 
 // Equal returns true, iff the given address is of type Address and their public keys are equal.
