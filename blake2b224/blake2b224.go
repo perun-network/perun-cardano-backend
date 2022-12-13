1859a2d665a8a31b398e4b5b23d53f7ce963b796
Jan Bormet <jan.bormet@bormet.info>
2022-12-13 14:19:14 +0100

    blake2b224: Add support for the blake2b-224 hash function
    This function is frequently used in Cardano (e.g. for public key hashes).
    We want to add convenient support, as this will be used across this backend.

---
+++ blake2b224/blake2b224.go
@@ -0,0 +1,24 @@
+package blake2b224
+
+import (
+	"fmt"
+	"golang.org/x/crypto/blake2b"
+)
+
+// Size224 is the length of blake2b224 hashes in bytes.
+const Size224 = 224 / 8
+
+// Sum224 returns the blake2b224 hash of the data.
+func Sum224(data []byte) ([Size224]byte, error) {
+	blake2b224, err := blake2b.New(Size224, nil)
+	if err != nil {
+		return [Size224]byte{}, fmt.Errorf("unable to instantiate blake2b224: %w", err)
+	}
+	_, err = blake2b224.Write(data)
+	if err != nil {
+		return [Size224]byte{}, fmt.Errorf("unable to compute blake2b224 hash of data: %w", err)
+	}
+	var res [Size224]byte
+	copy(res[:], blake2b224.Sum(nil))
+	return res, nil
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
 	perun.network/go-perun v0.10.6
 )
 
@@ -12,6 +13,7 @@ require (
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 	polycry.pt/poly-go v0.0.0-20220222131629-aa4bdbaab60b // indirect
 )
