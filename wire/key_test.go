a563cdc828a6659c22a6e85dfeed7d668b334fde
Jan Bormet <jan.bormet@bormet.info>
2022-12-20 13:46:36 +0100

    wire: Group similar tests together

--- wire/key_test.go
+++ wire/key_test.go
@@ -1,44 +1,13 @@
 package wire_test
 
 import (
-	"encoding/hex"
 	"github.com/stretchr/testify/require"
-	"perun.network/perun-cardano-backend/wallet/address"
 	"perun.network/perun-cardano-backend/wallet/test"
 	"perun.network/perun-cardano-backend/wire"
 	pkgtest "polycry.pt/poly-go/test"
 	"testing"
 )
 
-func TestMakePubKey(t *testing.T) {
-	rng := pkgtest.Prng(t)
-	testAddr := test.MakeRandomAddress(rng)
-
-	expected := wire.PubKey{Hex: hex.EncodeToString(testAddr.GetPubKeySlice())}
-	actual := wire.MakePubKey(testAddr)
-	require.Equal(t, expected, actual, "PubKey not as expected")
-}
-
-func TestPubKey_Decode(t *testing.T) {
-	const maxInvalidPubKeyLength = 128
-
-	rng := pkgtest.Prng(t)
-	expected := test.MakeRandomAddress(rng)
-	uut := wire.PubKey{Hex: hex.EncodeToString(expected.GetPubKeySlice())}
-	actual, err := uut.Decode()
-	require.NoError(t, err, "unexpected error when decoding public key")
-	require.Equal(t, expected, actual, "decoded address is wrong")
-
-	var invalidPubKeyBytes []byte
-	if rng.Int()%2 == 0 {
-		invalidPubKeyBytes = make([]byte, rng.Intn(address.PubKeyLength))
-	} else {
-		invalidPubKeyBytes = make([]byte, rng.Intn(maxInvalidPubKeyLength-address.PubKeyLength)+address.PubKeyLength+1)
-	}
-	uut = wire.PubKey{Hex: hex.EncodeToString(invalidPubKeyBytes)}
-	_, err = uut.Decode()
-	require.Errorf(t, err, "failed to return error when decoding PubKey with invalid pubKeyLength %d", len(invalidPubKeyBytes))
-}
 func TestPubKey(t *testing.T) {
 	rng := pkgtest.Prng(t)
 	expected := test.MakeRandomAddress(rng)
--- wire/remote_test.go
+++ wire/remote_test.go
@@ -10,81 +10,161 @@ import (
 	"testing"
 )
 
-func TestMakeSigningRequest(t *testing.T) {
+func TestMake(t *testing.T) {
 	rng := pkgtest.Prng(t)
 
 	const maxMessageLength = 0x100
-	message := make([]byte, maxMessageLength)
-	rng.Read(message)
-	addr := test.MakeRandomAddress(rng)
 
-	expected := wire.SigningRequest{
-		PubKey:  wire.MakePubKey(addr),
-		Message: hex.EncodeToString(message),
+	randomSigningRequestTest := func() func(*testing.T) {
+		referenceAddress := test.MakeRandomAddress(rng)
+		referenceMessage := make([]byte, rng.Intn(maxMessageLength))
+		rng.Read(referenceMessage)
+
+		return func(t *testing.T) {
+			t.Parallel()
+			expected := wire.SigningRequest{
+				PubKey:  wire.MakePubKey(referenceAddress),
+				Message: hex.EncodeToString(referenceMessage),
+			}
+			actual := wire.MakeSigningRequest(referenceAddress, referenceMessage)
+			require.Equal(t, expected, actual, "signing request is not as expected")
+		}
 	}
-	actual := wire.MakeSigningRequest(addr, message)
-	require.Equal(t, expected, actual, "signing request is not as expected")
-}
+	randomVerificationRequestTest := func() func(*testing.T) {
+		referenceAddress := test.MakeRandomAddress(rng)
+		referenceSignature := make(wallet.Sig, wire.SignatureLength)
+		rng.Read(referenceSignature)
+		referenceMessage := make([]byte, rng.Intn(maxMessageLength))
+		rng.Read(referenceMessage)
 
-func TestMakeVerificationRequest(t *testing.T) {
-	rng := pkgtest.Prng(t)
-
-	const maxMessageLength = 0x100
-	message := make([]byte, maxMessageLength)
-	rng.Read(message)
-	signature := make(wallet.Sig, wire.SignatureLength)
-	rng.Read(signature)
-	addr := test.MakeRandomAddress(rng)
-
-	expected := wire.VerificationRequest{
-		Signature: wire.MakeSignature(signature),
-		PubKey:    wire.MakePubKey(addr),
-		Message:   hex.EncodeToString(message),
+		return func(t *testing.T) {
+			t.Parallel()
+			expected := wire.VerificationRequest{
+				Signature: wire.MakeSignature(referenceSignature),
+				PubKey:    wire.MakePubKey(referenceAddress),
+				Message:   hex.EncodeToString(referenceMessage),
+			}
+			actual := wire.MakeVerificationRequest(referenceSignature, referenceAddress, referenceMessage)
+			require.Equal(t, expected, actual, "signature request is not as expected")
+		}
 	}
-	actual := wire.MakeVerificationRequest(signature, addr, message)
-	require.Equal(t, expected, actual, "signature request is not as expected")
-}
 
-func TestMakeSignature(t *testing.T) {
-	rng := pkgtest.Prng(t)
-	testSig := make(wallet.Sig, wire.SignatureLength)
-	rng.Read(testSig)
-	expected := wire.Signature{Hex: hex.EncodeToString(testSig)}
-	actual := wire.MakeSignature(testSig)
-	require.Equal(t, expected, actual, "signature is not as expected")
-}
+	randomKeyAvailabilityRequestTest := func() func(*testing.T) {
+		referenceAddress := test.MakeRandomAddress(rng)
 
-func TestMakeKeyAvailabilityRequest(t *testing.T) {
-	rng := pkgtest.Prng(t)
-	addr := test.MakeRandomAddress(rng)
-	expected := wire.MakePubKey(addr)
-	actual := wire.MakeKeyAvailabilityRequest(addr)
-	require.Equal(t, expected, actual, "KeyAvailabilityRequest is not as expected")
-}
-
-func TestSigningResponse_Decode(t *testing.T) {
-	rng := pkgtest.Prng(t)
-	expected := make(wallet.Sig, wire.SignatureLength)
-	rng.Read(expected)
-	uut := wire.MakeSignature(expected)
-	actual, err := uut.Decode()
-	require.NoError(t, err, "unexpected error when Decoding valid signature")
-	require.Equal(t, expected, actual, "decoded signature is not as expected")
-
-	const maxInvalidSigLength = 0x100
-	var invalidSig wallet.Sig
-	if rng.Int()%2 == 0 {
-		invalidSig = make(wallet.Sig, rng.Intn(wire.SignatureLength))
-	} else {
-		invalidSig = make(wallet.Sig, rng.Intn(maxInvalidSigLength-wire.SignatureLength)+wire.SignatureLength+1)
+		return func(t *testing.T) {
+			t.Parallel()
+			expected := wire.MakePubKey(referenceAddress)
+			actual := wire.MakeKeyAvailabilityRequest(referenceAddress)
+			require.Equal(t, expected, actual, "KeyAvailabilityRequest is not as expected")
+		}
 	}
-	rng.Read(invalidSig)
-	uut = wire.MakeSignature(invalidSig)
-	_, err = uut.Decode()
-	require.Errorf(
-		t,
-		err,
-		"failed to return error when decoding invalid signature of length %d from SigningResponse",
-		len(invalidSig),
-	)
+
+	randomSignatureTest := func() func(*testing.T) {
+		referenceSignature := make(wallet.Sig, wire.SignatureLength)
+		rng.Read(referenceSignature)
+
+		return func(t *testing.T) {
+			t.Parallel()
+			expected := wire.Signature{Hex: hex.EncodeToString(referenceSignature)}
+			actual := wire.MakeSignature(referenceSignature)
+			require.Equal(t, expected, actual, "signature is not as expected")
+		}
+	}
+
+	randomPubKeyTest := func() func(*testing.T) {
+		referenceAddress := test.MakeRandomAddress(rng)
+
+		return func(t *testing.T) {
+			t.Parallel()
+			expected := wire.PubKey{Hex: hex.EncodeToString(referenceAddress.GetPubKeySlice())}
+			actual := wire.MakePubKey(referenceAddress)
+			require.Equal(t, expected, actual, "PubKey not as expected")
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		t.Run("MakeSigningRequest", randomSigningRequestTest())
+		t.Run("MakeVerificationRequest", randomVerificationRequestTest())
+		t.Run("MakeKeyAvailabilityRequest", randomKeyAvailabilityRequestTest())
+		t.Run("MakeSignature", randomSignatureTest())
+		t.Run("MakePubKey", randomPubKeyTest())
+	}
+}
+
+func TestDecode(t *testing.T) {
+	rng := pkgtest.Prng(t)
+
+	randomSigningResponseTest := func() func(*testing.T) {
+		referenceSignature := make(wallet.Sig, wire.SignatureLength)
+		rng.Read(referenceSignature)
+
+		return func(t *testing.T) {
+			t.Parallel()
+			uut := wire.MakeSignature(referenceSignature)
+			actual, err := uut.Decode()
+			require.NoError(t, err, "unexpected error when Decoding valid signature")
+			require.Equal(t, referenceSignature, actual, "decoded signature is not as expected")
+		}
+	}
+	randomSigningResponseInvalidTest := func(invalidSig wallet.Sig) func(*testing.T) {
+		return func(t *testing.T) {
+			t.Parallel()
+			uut := wire.MakeSignature(invalidSig)
+			_, err := uut.Decode()
+			require.Errorf(
+				t,
+				err,
+				"failed to return error when decoding invalid signature of length %d from SigningResponse",
+				len(invalidSig),
+			)
+		}
+	}
+
+	randomPubKeyTest := func() func(*testing.T) {
+		referenceAddress := test.MakeRandomAddress(rng)
+
+		return func(t *testing.T) {
+			t.Parallel()
+			uut := wire.PubKey{Hex: hex.EncodeToString(referenceAddress.GetPubKeySlice())}
+			actual, err := uut.Decode()
+			require.NoError(t, err, "unexpected error when decoding public key")
+			require.Equal(t, referenceAddress, actual, "decoded address is wrong")
+		}
+	}
+
+	randomPubKeyInvalidTest := func(invalidPubKeyBytes []byte) func(*testing.T) {
+		return func(t *testing.T) {
+			t.Parallel()
+			uut := wire.PubKey{Hex: hex.EncodeToString(invalidPubKeyBytes)}
+			_, err := uut.Decode()
+			require.Errorf(
+				t,
+				err,
+				"failed to return error when decoding PubKey with invalid pubKeyLength %d",
+				len(invalidPubKeyBytes),
+			)
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		t.Run("SigningResponse Decode - Valid", randomSigningResponseTest())
+
+		t.Run(
+			"SigningResponse Decode - Invalid - Signature too short",
+			randomSigningResponseInvalidTest(test.MakeTooShortSignature(rng)),
+		)
+		t.Run(
+			"SigningResponse Decode - Invalid - Signature too long",
+			randomSigningResponseInvalidTest(test.MakeTooLongSignature(rng)))
+		t.Run("PubKey Decode - Valid", randomPubKeyTest())
+		t.Run(
+			"PubKey Decode - Invalid - PubKey too short",
+			randomPubKeyInvalidTest(test.MakeTooFewPublicKeyBytes(rng)),
+		)
+		t.Run(
+			"PubKey Decode - Invalid - PubKey too long",
+			randomPubKeyInvalidTest(test.MakeTooManyPublicKeyBytes(rng)),
+		)
+	}
 }
