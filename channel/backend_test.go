c0b23ec4e8f302d93e5e03d546b3cfd0e446ba8b
Jan Bormet <jan.bormet@bormet.info>
2023-02-03 11:24:48 +0100

    channel_test: Add generic go-perun channel/Backend tests

---
+++ channel/backend_test.go
@@ -0,0 +1,77 @@
+package channel_test
+
+import (
+	"math/big"
+	"math/rand"
+	gpchannel "perun.network/go-perun/channel"
+	gptest "perun.network/go-perun/channel/test"
+	gpwallet "perun.network/go-perun/wallet"
+	"perun.network/perun-cardano-backend/channel"
+	"perun.network/perun-cardano-backend/channel/types"
+	"perun.network/perun-cardano-backend/wallet"
+	"perun.network/perun-cardano-backend/wallet/test"
+	pkgtest "polycry.pt/poly-go/test"
+	"testing"
+)
+
+func setup(rng *rand.Rand) *gptest.Setup {
+
+	w := wallet.NewRemoteWallet(GenericTestRemote)
+	acc, err := w.Unlock(&GenericTestRemote.AvailableAddress)
+	newRandomAddress := func() gpwallet.Address {
+		rAddr := test.MakeRandomAddress(rng)
+		return &rAddr
+	}
+	newParamsAndState := func(opts ...gptest.RandomOpt) (*gpchannel.Params, *gpchannel.State) {
+		return gptest.NewRandomParamsAndState(
+			rng,
+			gptest.WithoutApp().
+				Append(gptest.WithParts(newRandomAddress(), newRandomAddress())).
+				Append(gptest.WithLedgerChannel(true)).
+				Append(gptest.WithVirtualChannel(false)).
+				Append(gptest.WithAssets(channel.Asset)).
+				Append(gptest.WithBalancesInRange(new(big.Int).SetUint64(0), types.MaxBalance)).
+				Append(opts...),
+		)
+	}
+	p1, s1 := newParamsAndState()
+	// We need this because `s1` and `s2` must differ IN EVERY FIELD for the verification tests to work.
+	p2, s2 := newParamsAndState(gptest.WithIsFinal(!s1.IsFinal))
+	if err != nil {
+		panic(err)
+	}
+	return &gptest.Setup{
+		Params:        p1,
+		Params2:       p2,
+		State:         s1,
+		State2:        s2,
+		Account:       acc,
+		RandomAddress: newRandomAddress,
+	}
+}
+
+var GenericTestRemote *test.GenericRemote
+
+type main struct{}
+
+func (main) Name() string {
+	return "TestMain"
+}
+
+func TestMain(m *testing.M) {
+	// Setting up the go-perun backend values for the generic tests.
+	rng := pkgtest.Prng(main{})
+	addr := test.MakeRandomAddress(rng)
+	GenericTestRemote = test.NewGenericRemote(addr, rng)
+	wb := wallet.MakeRemoteBackend(GenericTestRemote)
+	gpwallet.SetBackend(wb)
+	channel.SetWalletBackend(wb)
+	gpchannel.SetBackend(channel.Backend)
+
+	m.Run()
+}
+
+func TestBackend(t *testing.T) {
+	rng := pkgtest.Prng(t)
+	gptest.GenericBackendTest(t, setup(rng), gptest.IgnoreApp, gptest.IgnoreAssets)
+}
