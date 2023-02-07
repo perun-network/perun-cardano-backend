6f20367dc2f0b1b4778368f9b89056cb11338b98
Jan Bormet <jan.bormet@bormet.info>
2023-02-07 09:45:15 +0100

    wire: Add ChannelDatum type

--- wire/channel_datum.go
+++ wire/channel_datum.go
@@ -1,6 +1,9 @@
 package wire
 
-import "time"
+import (
+	"perun.network/perun-cardano-backend/channel/types"
+	"time"
+)
 
 type ChannelDatum struct {
 	ChannelParameters ChannelParameters `json:"channelParameters"`
@@ -9,4 +12,19 @@ type ChannelDatum struct {
 	Funding           []uint64          `json:"funding"`
 	Funded            bool              `json:"funded"`
 	Disputed          bool              `json:"disputed"`
+}
+
+func (c ChannelDatum) Decode() (types.ChannelDatum, error) {
+	p, err := c.ChannelParameters.Decode()
+	if err != nil {
+		return types.ChannelDatum{}, err
+	}
+	return types.ChannelDatum{
+		ChannelParameters: p,
+		ChannelState:      c.ChannelState.Decode(),
+		Time:              c.Time,
+		FundingBalances:   c.Funding,
+		Funded:            c.Funded,
+		Disputed:          c.Disputed,
+	}, nil
 }
