056ccb5cf897eeab753768c8ca6a47048dfb2202
Jan Bormet <jan.bormet@bormet.info>
2023-03-13 12:17:53 +0100

    types: Rework event types

--- channel/types/event.go
+++ channel/types/event.go
@@ -17,52 +17,34 @@ package types
 import "perun.network/go-perun/channel"
 
 const (
-	StartedTag    = "Started"
-	DepositedTag  = "Deposited"
-	DisputedTag   = "Disputed"
-	ProgressedTag = "Progressed"
-	ConcludedTag  = "Concluded"
-	WithdrawnTag  = "Withdrawn"
+	CreatedTag   = "Created"
+	DepositedTag = "Deposited"
+	DisputedTag  = "Disputed"
+	ConcludedTag = "Concluded"
 )
 
 // TODO: Figure out what to return on AdjudicatorEvent.Version(), if there is no concept of a state version for that event
 
 type (
-	Started struct {
-		ChannelID    ID
-		ChannelDatum ChannelDatum
+	Created struct {
+		ChannelID ID
+		NewDatum  ChannelDatum
 	}
 	Deposited struct {
-		ChannelID    ID
-		ChannelDatum ChannelDatum
+		ChannelID ID
+		OldDatum  ChannelDatum
+		NewDatum  ChannelDatum
 	}
 	Disputed struct {
-		ChannelID    ID
-		ChannelDatum ChannelDatum
-	}
-	Progressed struct {
-		ChannelID    ID
-		ChannelDatum ChannelDatum
+		ChannelID ID
+		OldDatum  ChannelDatum
+		NewDatum  ChannelDatum
 	}
 	Concluded struct {
 		ChannelID ID
-	}
-	Withdrawn struct {
-		ChannelID ID
+		OldDatum  ChannelDatum
 	}
 )
-
-func (w Withdrawn) ID() channel.ID {
-	return w.ChannelID
-}
-
-func (w Withdrawn) Timeout() channel.Timeout {
-	return nil
-}
-
-func (w Withdrawn) Version() uint64 {
-	return 0
-}
 
 func (c Concluded) ID() channel.ID {
 	return c.ChannelID
@@ -76,28 +58,16 @@ func (c Concluded) Version() uint64 {
 	return 0
 }
 
-func (p Progressed) ID() channel.ID {
-	return p.ChannelID
-}
-
-func (p Progressed) Timeout() channel.Timeout {
-	return &channel.TimeTimeout{Time: p.ChannelDatum.Time.Add(p.ChannelDatum.ChannelParameters.Timeout)}
-}
-
-func (p Progressed) Version() uint64 {
-	return p.ChannelDatum.ChannelState.Version
-}
-
 func (d Disputed) ID() channel.ID {
 	return d.ChannelID
 }
 
 func (d Disputed) Timeout() channel.Timeout {
-	return &channel.TimeTimeout{Time: d.ChannelDatum.Time.Add(d.ChannelDatum.ChannelParameters.Timeout)}
+	return &channel.TimeTimeout{Time: d.NewDatum.Time.Add(d.NewDatum.ChannelParameters.Timeout)}
 }
 
 func (d Disputed) Version() uint64 {
-	return d.ChannelDatum.ChannelState.Version
+	return d.NewDatum.ChannelState.Version
 }
 
 func (d Deposited) ID() channel.ID {
@@ -112,14 +82,14 @@ func (d Deposited) Version() uint64 {
 	return 0
 }
 
-func (s Started) ID() channel.ID {
+func (s Created) ID() channel.ID {
 	return s.ChannelID
 }
 
-func (s Started) Timeout() channel.Timeout {
+func (s Created) Timeout() channel.Timeout {
 	return nil
 }
 
-func (s Started) Version() uint64 {
+func (s Created) Version() uint64 {
 	return 0
 }
